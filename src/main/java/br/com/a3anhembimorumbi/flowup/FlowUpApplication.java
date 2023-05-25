3f8fc09b4160264406209a614175a8e201a97204
GabrielF-S <gabrielboni2008@hotmail.com>
2023-05-24 21:45:35 -0300

    Commit do projeto Maven

---
+++ src/main/java/br/com/a3anhembimorumbi/flowup/FlowUpApplication.java
@@ -0,0 +1,13 @@
+package br.com.a3anhembimorumbi.flowup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowUpApplication.class, args);
+	}
+
+}
