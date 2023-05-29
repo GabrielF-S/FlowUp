b86400635ea1d6555cb86037921f9cd7786397cc
GabrielF-S <gabrielboni2008@hotmail.com>
2023-05-28 21:54:06 -0300

    Adicionado testes para a criação dos demais tipos de usuarios

--- src/test/java/br/com/a3anhembimorumbi/flowup/model/UsuarioAdminTeste.java
+++ src/test/java/br/com/a3anhembimorumbi/flowup/model/UsuarioAdminTeste.java
@@ -20,7 +20,7 @@ public class UsuarioAdminTeste {
 	}
 	
 	@Test
-	public void when_createComunUSer_notReturnOtherTypeUser() {
+	public void when_createComunUSer_notReturnUserAdminType() {
 		
 		//cenario
 		UsuarioAdmin admin = new UsuarioAdmin("Marcelo", "Soares", "marso", "mar123456");
@@ -30,6 +30,61 @@ public class UsuarioAdminTeste {
 		
 		//resultado
 		Assertions.assertThat(usuaurioComun).isNotInstanceOf(UsuarioAdmin.class);
+		
+	}
+	@Test
+	public void when_createComunUSer_notReturnUserGestorType() {
+		
+		//cenario
+		UsuarioAdmin admin = new UsuarioAdmin("Marcelo", "Soares", "marso", "mar123456");
+		
+		//ação
+		UsuarioComum usuaurioComun = admin.CadastrarusuarioComun("Arthur","Perereira", "artpe", "art123456");
+		
+		//resultado
+		Assertions.assertThat(usuaurioComun).isNotInstanceOf(UsuarioGestor.class);
+		
+	}
+	
+	@Test
+	public void when_createGestorUSer_returnGestorUser() {
+		
+		//cenario
+		UsuarioAdmin admin = new UsuarioAdmin("Marcelo", "Soares", "marso", "mar123456");
+		
+		//ação
+		UsuarioGestor usuarioGestor = admin.CadastrarusuarioGestor("Arthur","Perereira", "artpe", "art123456");
+		
+		//resultado
+		Assertions.assertThat(usuarioGestor).isInstanceOf(UsuarioGestor.class);
+		
+	}
+	
+	@Test
+	public void when_createGestorUSer_notReturnUserComunType() {
+		
+		//cenario
+		UsuarioAdmin admin = new UsuarioAdmin("Marcelo", "Soares", "marso", "mar123456");
+		
+		//ação
+		UsuarioGestor usuarioGestor = admin.CadastrarusuarioGestor("Arthur","Perereira", "artpe", "art123456");
+		
+		//resultado
+		Assertions.assertThat(usuarioGestor).isNotInstanceOf(UsuarioComum.class);
+		
+	}
+	
+	@Test
+	public void when_createGestorUSer_notReturnUserAdminType() {
+		
+		//cenario
+		UsuarioAdmin admin = new UsuarioAdmin("Marcelo", "Soares", "marso", "mar123456");
+		
+		//ação
+		UsuarioGestor usuarioGestor = admin.CadastrarusuarioGestor("Arthur","Perereira", "artpe", "art123456");
+		
+		//resultado
+		Assertions.assertThat(usuarioGestor).isNotInstanceOf(UsuarioAdmin.class);
 		
 	}
 	
