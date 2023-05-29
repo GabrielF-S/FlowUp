ea029200579d9bf17f4dba2ba2162a8fc7989d99
GabrielF-S <gabrielboni2008@hotmail.com>
2023-05-28 23:37:28 -0300

    Criado classe Area e metodos para verificar area total e area disponivel

---
+++ src/main/java/br/com/a3anhembimorumbi/flowup/model/Area.java
@@ -0,0 +1,82 @@
+package br.com.a3anhembimorumbi.flowup.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Area {
+	
+	private int id;
+	
+	private String nome;
+	
+	private String regiao;
+	
+	private long area;
+	
+	private List<Plantacao> plantacoes;
+
+	public Area(String nome, String regiao, long area) {
+		super();
+		setNome(nome);
+		setRegiao(regiao);
+		setArea(area);
+		plantacoes = new ArrayList<>();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRegiao() {
+		return regiao;
+	}
+
+	public void setRegiao(String regiao) {
+		this.regiao = regiao;
+	}
+
+	public long getArea() {
+		return area;
+	}
+
+	public void setArea(long area) {
+		this.area = area;
+	}
+	
+	public void adicionarPlantacao(Plantacao plantacao) {
+		plantacoes.add(plantacao);
+	}
+	
+	
+	public long areaPlantacoes() {
+		long areaUtilizada = 0;
+		
+		for(Plantacao plan : plantacoes) {
+			areaUtilizada += plan.getArea();
+		}
+		
+		return areaUtilizada;
+		
+		
+	}
+
+	public long getAreaTotalDisponivel() {
+	
+		long areaTotalDisponivel = getArea() - areaPlantacoes();
+		
+		return areaTotalDisponivel;
+	}
+	
+	
+	
+
+}
