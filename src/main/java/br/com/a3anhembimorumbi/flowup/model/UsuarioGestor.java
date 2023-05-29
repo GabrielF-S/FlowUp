8165efb0815ee01e4c1a7a24842fdef92c81c889
GabrielF-S <gabrielboni2008@hotmail.com>
2023-05-28 21:55:38 -0300

    Criado a classe UsuarioGestor

---
+++ src/main/java/br/com/a3anhembimorumbi/flowup/model/UsuarioGestor.java
@@ -0,0 +1,9 @@
+package br.com.a3anhembimorumbi.flowup.model;
+
+public class UsuarioGestor extends Usuario{
+
+	public UsuarioGestor(String nome, String sobrenome, String login, String senha) {
+		super(nome, sobrenome, login, senha);
+	}
+
+}
