b4adcbf66c5f3d5753664fb3e6511a1feaf20a7b
GabrielF-S <gabrielboni2008@hotmail.com>
2023-05-28 23:31:33 -0300

    Adicionado metodos de criação de Areas e Plantacoes

--- src/main/java/br/com/a3anhembimorumbi/flowup/model/UsuarioGestor.java
+++ src/main/java/br/com/a3anhembimorumbi/flowup/model/UsuarioGestor.java
@@ -6,4 +6,16 @@ public class UsuarioGestor extends Usuario{
 		super(nome, sobrenome, login, senha);
 	}
 
+	public Area CadastrarAreaTotal(String nome, String regiao, long area) {
+		Area espaco = new Area(nome, regiao, area);
+
+		return espaco;
+	}
+
+	public Plantacao CadastrarPlantacao(String nome, long area, String plantacao, int qtdeAguaAno, Area local) {
+		Plantacao plan = new Plantacao(nome, area, plantacao, qtdeAguaAno, local);
+		
+		return plan;
+	}
+
 }
