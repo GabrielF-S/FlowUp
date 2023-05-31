74a6b5de6e82f979e3f9341bf3de6f6ec9985838
GabrielF-S <gabrielboni2008@hotmail.com>
2023-05-30 21:27:00 -0300

    Adicionado testes para Validar  e ativar rotinas

--- src/test/java/br/com/a3anhembimorumbi/flowup/model/entities/UsuarioGestorTeste.java
+++ src/test/java/br/com/a3anhembimorumbi/flowup/model/entities/UsuarioGestorTeste.java
@@ -3,35 +3,67 @@ package br.com.a3anhembimorumbi.flowup.model.entities;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import br.com.a3anhembimorumbi.flowup.model.entities.Plantacao;
-import br.com.a3anhembimorumbi.flowup.model.entities.Terreno;
-import br.com.a3anhembimorumbi.flowup.model.entities.UsuarioGestor;
+import br.com.a3anhembimorumbi.flowup.model.Frequencia;
 
 public class UsuarioGestorTeste {
 	
+
 	@Test
 	public void when_createArea_then_returnArea() {
-		//cenario
-		UsuarioGestor usuario = new UsuarioGestor("Tomas","Shelbi", "thosh", "tho12345");
-		
-		//acao
-		Terreno terreno = usuario.CadastrarAreaTotal("Area1","Nordeste",200L);
-		
-		//resultado
+		// cenario
+		UsuarioGestor usuario = new UsuarioGestor("Tomas", "Shelbi", "thosh", "tho12345");
+
+		// acao
+		Terreno terreno = usuario.CadastrarAreaTotal("Area1", "Nordeste", 200L);
+
+		// resultado
 		Assertions.assertThat(terreno).isInstanceOf(Terreno.class);
-		
+
 	}
+
 	@Test
 	public void when_createPlantcao_then_returnPlantcao() {
-		//cenario
-		UsuarioGestor usuario = new UsuarioGestor("Tomas","Shelbi", "thosh", "tho12345");
-		
-		Terreno terreno = usuario.CadastrarAreaTotal("Area1","Nordeste",200L);
-		//acao
+		// cenario
+		UsuarioGestor usuario = new UsuarioGestor("Tomas", "Shelbi", "thosh", "tho12345");
+
+		Terreno terreno = usuario.CadastrarAreaTotal("Area1", "Nordeste", 200L);
+		// acao
 		Plantacao plantacao = usuario.CadastrarPlantacao("PLantacao1", 100L, "Pepino", 42, terreno);
-		
-		//resultado
+
+		// resultado
 		Assertions.assertThat(plantacao).isInstanceOf(Plantacao.class);
+
+	}
+
+	@Test
+	public void when_approval_rotina() {
+		// cenario
+		UsuarioGestor usuario = new UsuarioGestor("Tomas", "Shelbi", "thosh", "tho12345");
+		Terreno terreno = usuario.CadastrarAreaTotal("Area1", "Nordeste", 200L);
+		Plantacao plantacao = usuario.CadastrarPlantacao("PLantacao1", 100L, "Pepino", 42, terreno);
+		Rotina rot = new Rotina("Rotina1", plantacao, Frequencia.DIARIO);
+		// acao
+		
+	    usuario.aprovarRotinas(rot);
+
+		// retorno
+	    Assertions.assertThat(rot.isAprovado()).isTrue();
+	   
+	}
+	@Test
+	public void when_desativar_rotina() {
+		// cenario
+		UsuarioGestor usuario = new UsuarioGestor("Tomas", "Shelbi", "thosh", "tho12345");
+		Terreno terreno = usuario.CadastrarAreaTotal("Area1", "Nordeste", 200L);
+		Plantacao plantacao = usuario.CadastrarPlantacao("PLantacao1", 100L, "Pepino", 42, terreno);
+		Rotina rot = new Rotina("Rotina1", plantacao, Frequencia.DIARIO);
+		rot.setAtivado(true);
+		// acao
+		
+		usuario.desativarRotinas(rot);
+		
+		// retorno
+		Assertions.assertThat(rot.isAtivado()).isTrue();
 		
 	}
 
