d5d0b23d83d8ae1df7dcbf4eaa364c56285e4045
GabrielF-S <gabrielboni2008@hotmail.com>
2023-06-03 12:57:22 -0300

    Criado funcao para verificar o final de um ciclo de plantacao

--- src/main/java/br/com/a3anhembimorumbi/flowup/model/entities/Plantacao.java
+++ src/main/java/br/com/a3anhembimorumbi/flowup/model/entities/Plantacao.java
@@ -1,5 +1,8 @@
 package br.com.a3anhembimorumbi.flowup.model.entities;
 
+import java.time.LocalDate;
+
+import br.com.a3anhembimorumbi.flowup.model.service.EmailService;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Transient;
 
 @Entity
 public class Plantacao {
@@ -26,20 +30,38 @@ public class Plantacao {
 	@ManyToOne
 	private Terreno local;
 	
+	private long ciclo;
+	
+	private LocalDate inicio;
+	
+	private LocalDate finalCiclo;
+	@Transient
+	private EmailService email;
+
+
 	@OneToOne(cascade = CascadeType.PERSIST)
 	@JoinColumn(name ="rotina_id", unique = true)
 	private Rotina rotina;
 
 
-	public Plantacao(String nome, long area, String platacao, long qtdeAguaAno, Terreno local) {
+	public Plantacao(String nome, long area, String platacao, long qtdeAguaAno, Terreno local, long ciclo) {
 		super();
 		setNome(nome);
 		setArea(area);
 		setPlatacao(platacao);
 		setQtdeAguaAno(qtdeAguaAno);
 		setLocal(local);
+		setCiclo(ciclo);
 		local.adicionarPlantacao(this);
+		inicio =LocalDate.now();
+		
+		definirFinal();
+		
+		
+		
 	}
+
+
 
 
 	public int getId() {
@@ -96,7 +118,81 @@ public class Plantacao {
 	public void setLocal(Terreno local) {
 		this.local = local;
 	}
+
+
+	public long getCiclo() {
+		return ciclo;
+	}
+
+
+	public void setCiclo(long ciclo) {
+		this.ciclo = ciclo;
+	}
+
+
+	public LocalDate getInicio() {
+		return inicio;
+	}
+
+
+	public void setInicio(LocalDate inicio) {
+		this.inicio = inicio;
+	}
+
+
+	public Rotina getRotina() {
+		return rotina;
+	}
+
+
+	public void setRotina(Rotina rotina) {
+		this.rotina = rotina;
+	}
+
+	public LocalDate getFinalCiclo() {
+		return finalCiclo;
+	}
 	
+	
+	public void setFinalCiclo(LocalDate finalCiclo) {
+		this.finalCiclo = finalCiclo;
+	}
+
+	
+	private void definirFinal() {
+				
+		setFinalCiclo(getInicio().plusDays(getCiclo()));
+		
+	}
+	
+	public int checarCiclo() {
+		
+		LocalDate dataAtual = LocalDate.now();
+		
+		int cicloValido = dataAtual.compareTo(getFinalCiclo());
+			
+		return cicloValido;
+	}
+
+
+
+
+	public boolean notificarGestor() {
+		if(checarCiclo()>=0) {
+			return true;
+		}		
+		return false;
+	}
+
+
+
+
+//	public void enviarNotificacao(UsuarioGestor gestor, Plantacao plan) {
+//		if(notificarGestor()) {
+//			email.notificarFinalDeCiclo(gestor, plan);
+//		}
+//	}
+//	
 	
 
 	
